1bc389ab7d321a0321250239be46d1150c7546b1
PatKhongThong <phat19.kh@gmail.com>
2026-05-28 15:37:31 +0700

    dwas

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,8 +4,8 @@
 
 export const SUPABASE_CONFIG = {
   // Paste your Supabase project URL here (e.g. "https://yourproject.supabase.co")
-  url: "your-supabase-project-url-here",
+  url: "https://purasvifjrojawifvczv.supabase.co",
 
   // Paste your Supabase public anon key here (long string)
-  anonKey: "your-supabase-anon-key-here",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cmFzdmlmanJvamF3aWZ2Y3p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NTEzMDQsImV4cCI6MjA5NTUyNzMwNH0.O___vn0bKhjugzfjYBlXzy-rjYpFEZ7noaS6w9xhd0o",
 };
